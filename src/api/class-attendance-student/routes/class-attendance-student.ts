28295ab674942d6e283d1a5b5fddb6f7d0c7cd75
Brian Koh <briankpw@gmail.com>
2024-11-07 23:17:23 +0800

    add timezone

--- src/api/class-attendance-student/routes/class-attendance-student.ts
+++ src/api/class-attendance-student/routes/class-attendance-student.ts
@@ -1,19 +1,19 @@
 export default {
   routes: [
     {
-     method: 'GET',
-     path: '/class-attendance/recurring',
-     handler: 'class-attendance-student.recurring',
+      method: 'GET',
+      path: '/class-attendance/recurring',
+      handler: 'class-attendance-student.recurring',
+    },
+    {
+      method: 'GET',
+      path: '/class-attendance/history',
+      handler: 'class-attendance-student.attendanceHistory',
     },
     {
       method: 'GET',
       path: '/class-attendance/current',
       handler: 'class-attendance-student.currentAttendance',
-     },
-     {
-      method: 'GET',
-      path: '/class-attendance/history/student',
-      handler: 'class-attendance-student.attendanceHistory',
-     },
-  ],
+    }
+  ]
 };
